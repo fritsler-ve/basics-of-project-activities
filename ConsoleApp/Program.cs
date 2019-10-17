bcd2f2666b235a99ac3b33e99e1db87c375b0178
Fritsler Victor <fritsler.ve@yandex.ru>
2019-10-17 19:01:38 +0300

    17-10-19-19:00
    Есть функции, парсящие арифметические выражения, но выход за границы массива при подсчёте

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,163 @@
+﻿using Challenge;
+using Challenge.DataContracts;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        // ХУЛИ СМОТРИШЬ БЛЯДЬ!?
+
+        private static bool Priority(char input)
+        {
+            var prioritized = new List<char>() {'*', '/', '%'};
+            return prioritized.Contains(input);
+        }
+        
+        private static string Parse(string data)
+        {
+            var parsedData = new List<string>();
+            var currentNumber = "";
+            for (var i = 0; i < data.Length; i++)
+            {
+                if (Char.IsDigit(data[i]))
+                {
+                    currentNumber += data[i];
+                }
+                else
+                {
+                    parsedData.Add(currentNumber);
+                    currentNumber = "";
+                    parsedData.Add(data[i] + "");
+                }
+            }
+
+            return SolveExpression(ref parsedData);
+        }
+
+        private static string SolveExpression(ref List<string> data)
+        {
+            var usedElements = new List<bool>();
+            for (int i = 0; i < data.Count; i++)
+            {
+                usedElements.Add(false);
+            }
+            
+            char temp;
+            for (var i = 0; i < data.Count; i++) // ZALOOPA
+            {
+                temp = data[i][0];
+                if (char.IsDigit(temp) || !Priority(temp))
+                {
+                    continue;
+                }
+                usedElements[i - 1] = true; 
+                usedElements[i] = true; 
+                data[i + 1] = "" + SolveTwo(int.Parse(data[i - 1]), int.Parse(data[i + 1]), temp);
+            }
+
+            var length = data.Count;
+            for (var i = 0; i < length; i++)
+            {
+                if (usedElements[length - i])
+                {
+                    usedElements.RemoveAt(i);
+                    data.RemoveAt(i);
+                }
+            }
+            
+            for (var i = 0; i < data.Count; i++) // может показаться что вылезет за границы массива, 
+                                                 // и я не ебу, так ли это
+            {
+                temp = data[i][0];
+                if (char.IsDigit(temp))
+                {
+                    continue;
+                }
+                usedElements[i - 1] = true;
+                usedElements[i] = true; 
+                data[i + 1] = "" + SolveTwo(int.Parse(data[i - 1]), int.Parse(data[i + 1]), temp);
+            }
+            return data[data.Count - 1];
+        }
+
+        private static int SolveTwo(int first, int second, char operation)
+        {
+            if (operation == '+') return first + second;
+            if (operation == '-') return first - second;
+            if (operation == '*') return first * second;
+            if (operation == '/') return first / second;
+            return first % second;
+        }
+
+        static void Main(string[] args)
+        {
+            const string teamSecret = "jSVy9hRtt7bpflchqLGSc3l0iEgaRtp";
+            var challengeClient = new ChallengeClient(teamSecret);
+
+            const string challengeId = "projects-course";
+            Console.WriteLine($"Нажми ВВОД, чтобы получить информацию о челлендже {challengeId}");
+            Console.ReadLine();
+            var challenge = challengeClient.GetChallengeAsync(challengeId).Result;
+            Console.WriteLine(challenge.Description);
+            Console.WriteLine();
+            Console.WriteLine("----------------");
+            Console.WriteLine();
+
+            Console.WriteLine($"Нажми ВВОД, чтобы получить список взятых командой задач");
+            Console.ReadLine();
+            var allTasks = challengeClient.GetAllTasksAsync().Result;
+            for (int i = 0; i < allTasks.Count; i++)
+            {
+                allTasks[i].TeamAnswer = Parse(allTasks[i].Question);
+            }
+
+            for (int i = 0; i < allTasks.Count; i++)
+            {
+                var task = allTasks[i];
+                Console.WriteLine($"  Задание {i + 1}, статус {task.Status}");
+                Console.WriteLine($"                {task.Question}");
+                Console.WriteLine($"                {task.TeamAnswer}");
+            }
+
+            Console.WriteLine("----------------");
+            Console.WriteLine();
+/*
+            const string type = "math";
+            string round = challenge.Rounds[0].Id;
+            Console.WriteLine($"Нажми ВВОД, чтобы получить задачу типа {type}");
+            Console.ReadLine();
+            var newTask = challengeClient.AskNewTaskAsync(round, type).Result;
+            Console.WriteLine($"  Новое задание, статус {newTask.Status}");
+            Console.WriteLine($"  Формулировка: {newTask.UserHint}");
+            Console.WriteLine($"                {newTask.Question}");
+            Console.WriteLine();
+            Console.WriteLine("----------------");
+            Console.WriteLine();
+
+            string answer = SolveThree(newTask.Question) + "";
+            
+            Console.WriteLine($"Нажми ВВОД, чтобы ответить на полученную задачу самым правильным ответом: {answer}");
+            Console.ReadLine();
+            
+            var updatedTask = challengeClient.CheckTaskAnswerAsync(newTask.Id, answer).Result;
+            Console.WriteLine($"  Новое задание, статус {updatedTask.Status}");
+            Console.WriteLine($"  Формулировка:  {updatedTask.UserHint}");
+            Console.WriteLine($"                 {updatedTask.Question}");
+            updatedTask.TeamAnswer = SolveThree(updatedTask.Question) + "";
+            Console.WriteLine($"  Ответ команды: {SolveThree(updatedTask.Question)}");
+            Console.WriteLine();
+            if (updatedTask.Status == TaskStatus.Success)
+                Console.WriteLine($"Ура! Ответ угадан!");
+            else if (updatedTask.Status == TaskStatus.Failed)
+                Console.WriteLine($"Похоже ответ не подошел и задачу больше сдать нельзя...");*/
+            Console.WriteLine();
+            Console.WriteLine("----------------");
+            Console.WriteLine();
+
+            Console.WriteLine($"Нажми ВВОД, чтобы завершить работу программы");
+            Console.ReadLine();
+        }
+    }
+}
