541b1c91f039c20db395014eade8a80fede3f3ac
Daniil Gilmutdinov <fregire@mail.ru>
2019-10-18 20:14:25 +0500

    Исправил парсер

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -33,6 +33,9 @@ namespace ConsoleApp
                 }
             }
 
+            // Добавил  
+            parsedData.Add(currentNumber);
+
             return SolveExpression(ref parsedData);
         }
 
@@ -45,7 +48,7 @@ namespace ConsoleApp
             }
             
             char temp;
-            for (var i = 0; i < data.Count; i++) // ZALOOPA
+            for (var i = 1; i < data.Count - 1; i++) // ZALOOPA
             {
                 temp = data[i][0];
                 if (char.IsDigit(temp) || !Priority(temp))
@@ -58,19 +61,38 @@ namespace ConsoleApp
             }
 
             var length = data.Count;
-            for (var i = 0; i < length; i++)
+            var indexOfList = 0;
+            //Удаляет в массиве использованные элементы
+            while (true)
             {
-                if (usedElements[length - i])
+                if (usedElements[indexOfList])
+                {
+                    usedElements.RemoveAt(indexOfList);
+                    data.RemoveAt(indexOfList);
+                    if (indexOfList != 0)
+                        indexOfList--;
+                }
+                else
+                    indexOfList++;
+
+                if (indexOfList + 1 >= data.Count)
+                    break;
+            }
+            /*
+             * for (var i = 0; i < length; i++)
+            {
+                if (usedElements[])
                 {
                     usedElements.RemoveAt(i);
                     data.RemoveAt(i);
                 }
             }
+            */
             
-            for (var i = 0; i < data.Count; i++) // может показаться что вылезет за границы массива, 
+            for (var i = 1; i < data.Count - 1; i++) // может показаться что вылезет за границы массива, 
                                                  // и я не ебу, так ли это
             {
-                temp = data[i][0];
+                temp = data[i].Length > 1 ? data[i][1] : data[i][0];
                 if (char.IsDigit(temp))
                 {
                     continue;
@@ -123,7 +145,7 @@ namespace ConsoleApp
 
             Console.WriteLine("----------------");
             Console.WriteLine();
-/*
+
             const string type = "math";
             string round = challenge.Rounds[0].Id;
             Console.WriteLine($"Нажми ВВОД, чтобы получить задачу типа {type}");
@@ -136,7 +158,7 @@ namespace ConsoleApp
             Console.WriteLine("----------------");
             Console.WriteLine();
 
-            string answer = SolveThree(newTask.Question) + "";
+            string answer = Parse(newTask.Question) + "";
             
             Console.WriteLine($"Нажми ВВОД, чтобы ответить на полученную задачу самым правильным ответом: {answer}");
             Console.ReadLine();
@@ -145,13 +167,13 @@ namespace ConsoleApp
             Console.WriteLine($"  Новое задание, статус {updatedTask.Status}");
             Console.WriteLine($"  Формулировка:  {updatedTask.UserHint}");
             Console.WriteLine($"                 {updatedTask.Question}");
-            updatedTask.TeamAnswer = SolveThree(updatedTask.Question) + "";
-            Console.WriteLine($"  Ответ команды: {SolveThree(updatedTask.Question)}");
+            updatedTask.TeamAnswer = Parse(updatedTask.Question) + "";
+            Console.WriteLine($"  Ответ команды: {Parse(updatedTask.Question)}");
             Console.WriteLine();
             if (updatedTask.Status == TaskStatus.Success)
                 Console.WriteLine($"Ура! Ответ угадан!");
             else if (updatedTask.Status == TaskStatus.Failed)
-                Console.WriteLine($"Похоже ответ не подошел и задачу больше сдать нельзя...");*/
+                Console.WriteLine($"Похоже ответ не подошел и задачу больше сдать нельзя...");
             Console.WriteLine();
             Console.WriteLine("----------------");
             Console.WriteLine();
