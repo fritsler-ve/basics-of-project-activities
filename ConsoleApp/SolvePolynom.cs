3860497ef6431cf1247e2dcc507e8f2a8978bbed
Fritsler Victor <fritsler.ve@yandex.ru>
2019-10-24 18:50:49 +0300

    Добавлен парсер
    Корректность не гарантируется
    ГАРИТ ЖОПА ОТ ПИТОНА

--- ConsoleApp/SolvePolynom.cs
+++ ConsoleApp/SolvePolynom.cs
@@ -1,11 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
 
 namespace ConsoleApplication1
 {
     internal class SolvePolynom
     {
-        public static string SolvePolynomExp(List<double> Parameters)
+
+        public static string SolvePolynomExp(string str)
+        {
+            var Parameters = ParsePolynome(str);
+            var result = Solver(Parameters);
+            if (result != "no roots")
+            {
+                result.Replace(',', '.');
+            }
+
+            return result;
+        }
+        
+        public static List<double> ParsePolynome(string str) 
+        {
+            var digits = str.Split('+');
+            var result = new List<double>();
+            var degree = GetDegree(digits[0]);
+            foreach (var variable in digits)
+            {
+                var separatedParts = variable.Split('^');
+                var newDegree = GetDegree(variable);
+                while (newDegree != degree)
+                {
+                    result.Add(0);
+                    degree--;
+                }
+                result.Add(FindNumber(separatedParts[0]));
+                if (separatedParts[0].Contains("-"))
+                    result[result.Count - 1] *= (-1);
+                degree--;
+            }
+            while (degree != -1)
+            {
+                result.Add(0);
+                degree--;
+            }
+            return result;
+        }
+
+        public static double FindNumber(string str)
+        {
+            var coef = new StringBuilder();
+            for (var i = 0; i < str.Length; i++)
+            {
+                if (char.IsDigit(str[i]) || str[i] == '.')
+                    coef.Append(str[i]);
+            }
+            return (double.Parse(coef.ToString(), CultureInfo.InvariantCulture));
+        }
+
+        public static int GetDegree(string digits)
+        {
+            var degree = 0;
+            var first = digits.Split('^');
+            if (first.Length == 1)
+            {
+                if (first[0].Contains("x"))
+                    degree = 1;
+                else
+                    degree = 0;
+            }
+            else
+            {
+                degree = (int)Math.Round(FindNumber(first[1]));
+            }
+            return degree;
+        }
+        
+        public static string Solver(List<double> Parameters)
         {
             double left = -1;
             double right = 1;
